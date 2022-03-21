dd412baae28f0c23aa3d61385bbaede582519f2d
Nguyễn Thanh Trí <68100497+nguyenthanhtri1998@users.noreply.github.com>
2022-03-21 15:45:39 +0700

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,11 +1,19 @@
 firebase.auth().onAuthStateChanged((user)=>{
     if(!user){
         location.replace("index.html")
-    }else{
+    }
+   else if (user.email == "admin@admin.com") {
+      document.getElementById("adminpage").style.display = "block"
+      document.getElementById("user").innerHTML ="Hi," + user.email.substring(0,user.email.indexOf('@'));
+      
+    }
+    else {
         document.getElementById("user").innerHTML ="Hi," + user.email.substring(0,user.email.indexOf('@'));
+        document.getElementById("user2").innerHTML =user.email;
+   
+        
     }
   })
-  
   
   function logout(){
     firebase.auth().signOut()
