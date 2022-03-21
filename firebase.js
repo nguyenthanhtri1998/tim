87793c90ded9615229b312ce4914cb09ddb4a03b
Nguyễn Thanh Trí <68100497+nguyenthanhtri1998@users.noreply.github.com>
2022-03-21 15:51:05 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,4 @@
-// var firebaseConfig = {
-//     apiKey: "AIzaSyCV3-oocD0Fp_R87fMjqmkrMxiW-243eoY",
-//     authDomain: "chat-f3e17.firebaseapp.com",
-//     databaseURL: "https://chat-f3e17-default-rtdb.asia-southeast1.firebasedatabase.app",
-//     projectId: "chat-f3e17",
-//     storageBucket: "chat-f3e17.appspot.com",
-//     messagingSenderId: "725346365415",
-//     appId: "1:725346365415:web:869f0bbbbb2a4cb03d1742"
-// };
+
 var firebaseConfig = {
     apiKey: "AIzaSyDt6OVBwvPvAEAFpokPRXpfnA3zoYtI5hk",
   authDomain: "do-an-6aec8.firebaseapp.com",
